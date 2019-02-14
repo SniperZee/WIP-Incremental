ac58797680168a5b431edd3f4f522d5c6236a5d9
SniperZee <32908882+sniperzee@users.noreply.github.com>
2019-02-13 21:25:25 -0600

    Actually, why not rpg?

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,25 @@
-var game = {
-  thing: 0,
-  thingPerClick: 1
+var player = {
+  attack: 1,
+  gold: 0,
+};
+
+var monster = {
+  health: 10,
+  maxHealth: 10,
+  goldReward: 10
+};
+
+function attack() {
+  if(monster.health > 1){
+    monster.health -= player.attack
+  }else{
+    monster.health = monster.maxHealth
+    player.gold += monster.goldReward
+  }
+  updateValues();
 }
 
-function getThing() {
-  game.thing += game.thingPerClick
-  document.getElementById("things").innerHTML = game.thing + " Things"
+function updateValues(){
+  document.getElementById("HP").innerHTML = "Monster HP: " + monster.health + "/" + monster.maxHealth
+  document.getElementById("gold").innerHTML = "Gold: " + player.gold
 }

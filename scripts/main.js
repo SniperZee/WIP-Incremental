edbbc8819bed09f9387a4ef388ebcf4526dd06a8
SniperZee <32908882+SniperZee@users.noreply.github.com>
2019-02-13 21:32:06 -0600

    comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
-var player = {
+var player = { //player stats
   attack: 1,
   gold: 0,
 };
 
-var monster = {
+var monster = { //monster stats
   health: 10,
   maxHealth: 10,
   goldReward: 10
 };
 
-function attack() {
+function attack() { //attacks the monster, if monster has no hp, rewards gold
   if(monster.health > 1){
     monster.health -= player.attack
   }else{
@@ -19,7 +19,7 @@ function attack() {
   updateValues();
 }
 
-function updateValues(){
+function updateValues(){ //updates displayed values
   document.getElementById("HP").innerHTML = "Monster HP: " + monster.health + "/" + monster.maxHealth
   document.getElementById("gold").innerHTML = "Gold: " + player.gold
 }

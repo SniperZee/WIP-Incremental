04da05b71584803435707bf1b02bceb65fe653f7
SniperZee <32908882+sniperzee@users.noreply.github.com>
2019-02-17 15:54:53 -0600

    Player has health, and monsters attack.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 var player = { //player stats
+  health: 100,
+  maxHealth: 100,
   attack: 1,
   gold: 0,
   swordLevel: 0
@@ -7,6 +9,7 @@ var player = { //player stats
 var monster = { //monster stats
   health: 10,
   maxHealth: 10,
+  attack: 1,
   goldReward: 10
 };
 
@@ -16,13 +19,15 @@ function gimmeGold() { //debug
   updateValues();
 }
 
-function attack() { //attacks the monster, if monster has no hp, rewards gold
+function attack() { //attacks the monster. then the monster attacks., if monster has no hp, rewards gold
   monster.health -= player.attack + player.swordLevel;
   if(monster.health <= 0){
     console.log("Monster has died.");
     monster.health = monster.maxHealth;
     player.gold += monster.goldReward
-    }
+  }else{
+    player.health -= monster.attack;
+  }
   updateValues();
 }
 
@@ -34,7 +39,7 @@ function buySword() { //lets the player upgrade their sword if they have enough 
     player.attack += 1;
     swordPrice *= 2; //TODO: find a better way for this to scale.
     document.getElementById("shopkeeper").innerText = "Shopkeeper: Thank you! That'll definitely make you deal more damage."
-  }else if (randomNumber <= 0.15){ //yes i'm easter egg-ing already
+  }else if (randomNumber <= 0.10){ //yes i'm easter egg-ing already
     document.getElementById("shopkeeper").innerText = "Shopkeeper: Sorry hero, I can't give credit. Come back when you're a little... Mmmmmm... richer!"
   }else{ //tells the player that they don't have enough gold.
     document.getElementById("shopkeeper").innerText = "Shopkeeper: Sorry, you don't have the gold to afford that."
@@ -43,6 +48,7 @@ function buySword() { //lets the player upgrade their sword if they have enough 
 }
 
 function updateValues(){ //updates displayed values
+  document.getElementById("playerHealth").innerText = "Your HP: " + player.health + "/" + player.maxHealth;
   document.getElementById("monsterHealth").innerText = "Monster HP: " + monster.health + "/" + monster.maxHealth;
   document.getElementById("gold").innerText = "Gold: " + player.gold;
   document.getElementById("swordPrice").innerText= "Buy new sword for " + swordPrice + " gold";
